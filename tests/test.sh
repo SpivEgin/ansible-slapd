88bf4f5b16981543b6ed77283e0ed45748457bdd
Daniel Bacher <danielbacher@phill93.de>
2017-04-27 10:55:50 +0200

    Build Vagrant test environment

---
+++ tests/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if ! [ -f /etc/debian_version ] && ! which netstat >> /dev/null; then
+  yum -y install net-tools
+fi
+
+if netstat -tulpen | grep 363 ; then
+  echo "LDAP Protocol available"
+else
+  echo "LDAP Protocol not available"
+fi
+
+if netstat -tulpen | grep 636; then
+  echo "LDAPS Protocol available"
+else
+  echo "LDAPS Protocol not available"
+fi
+
+if [ -f /usr/local/var/ldapi ]; then
+  echo "LDAPI Protocol available"
+else
+  echo "LDAPI Protocol not available"
+fi
